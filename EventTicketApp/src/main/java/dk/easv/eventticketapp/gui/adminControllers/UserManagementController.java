91e387734b52df928f1ced159951096e72cf6e73
federicobac <federico.baccaro1@gmail.com>
2026-02-18 14:34:55 +0100

    Revert "Merge branch 'main' of https://github.com/dompon-easv/EventTicketsEASV"
    This reverts commit e20b6ba6aa80600cba32504ea0fe2dc9dbdaefb7, reversing
    changes made to ef0ad1586f56a2071978efcc814876948e7d5bb7.

--- EventTicketApp/src/main/java/dk/easv/eventticketapp/gui/adminControllers/UserManagementController.java
+++ EventTicketApp/src/main/java/dk/easv/eventticketapp/gui/adminControllers/UserManagementController.java
@@ -1,21 +1,4 @@
 package dk.easv.eventticketapp.gui.adminControllers;
 
-import dk.easv.eventticketapp.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 public class UserManagementController {
-    public void showAddUser(ActionEvent actionEvent) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("gui/AdminViews/AddEditUser.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
-    }
 }
